6162740c78fb4cc06aaa354910991bdacdbc4914
Hakan <hakanny33@gmail.com>
2024-08-09 22:01:13 +0300

    update1

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	name := "Hakan"
+	surname := "Yenidogan"
+
 	fmt.Println(Sum(1, 1))
+	fmt.Println(msg(name, surname))
 }
 
 func Sum(value1, value2 int64) int64 {
 	return value1 + value2
+}
+
+func msg(name, surname string) string {
+	return fmt.Sprintf("Hello %s, %s", name, surname)
 }
