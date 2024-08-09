43a89c4781ee2cb5387fe0aac91756f937473d93
Yigit Isik <ygtisik16@gmail.comgit config --global user.name ygtisik16@gmail.comgit config --global user.email ygtisik16@gmail.comgit config --global user.email ygtisik16@gmail.com>
2024-08-09 21:43:58 +0300

    slice-func-added

--- main.go
+++ main.go
@@ -4,8 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println(Sum(1, 1))
+	fmt.Println(sum(5, 10, 15, 31))
 }
 
 func Sum(value1, value2 int64) int64 {
 	return value1 + value2
+}
+
+var numbers = []int{1, 2, 3, 4, 5, 6, 7}
+
+func sum(number ...int) int {
+	sum := 0
+	for _, value := range number {
+		sum += value
+	}
+	return sum
 }
